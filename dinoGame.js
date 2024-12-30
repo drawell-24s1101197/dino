da67a1d039292d2f424762fff20af06be23313fd
drawell <eima_24s1101197@nnn.ed.jp>
2024-12-30 20:58:25 +0900

    createEntityメソッドを分離. move&drawメソッドのenemy部分を作成

--- dinoGame.js
+++ dinoGame.js
@@ -6,13 +6,14 @@ const ctx = canvas.getContext('2d');
 /**
  * @type {string[]}
  */
-const imgNames = ['bird', 'cactus', 'dino'];
+const imgNames = ['bird:enemy', 'cactus:enemy', 'dino:player'];
 
 class DinoGame {
     /**
      * @param {HTMLCanvasElement} canvas
      * @param {string[]} imageNames 
      */
+    r = (x) =>Math.floor(Math.random() * x);
     constructor(canvas, imageNames){
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
@@ -23,10 +24,12 @@ class DinoGame {
         this.score = 0;
         this.timer = null;
         let imgLoadCounter = 0;
-        for (const imgName of imageNames){
+        for (const imgDat of imageNames){
+            const [imgName, imgAttr] = imgDat.split(':');
             const imagePath = `image/${imgName}.png`;
             this.img[imgName] = new Image();
             this.img[imgName].src = imagePath;
+            this.img[imgName].dataset.entity = imgAttr;
             this.img[imgName].onload = () => {
                 imgLoadCounter++;
                 if(imgLoadCounter === imageNames.length){
@@ -42,20 +45,27 @@ class DinoGame {
         this.enemys = [];
         this.isGameOver = false;
         this.score = 0;
-        this.createEntity('dino');
+        this.createDino('dino');
         this.timer = setInterval(this.ticker, 30, this);
         document.onkeydown = (e) => this.keydown(e);
     }
+    /**
+     * 
+     * @param {DinoGame} self 
+     */
     ticker(self){
         self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
 
-        //TODO 敵キャラ作成
+        if(self.r(100) === 0)self.createEnemys('cactus');
+        if(self.r(200) === 0)self.createEnemys('bird');
 
         //キャラクタの移動
-        self.move('dino')
+        self.move('dino');
+        self.move('enemys');
 
         //キャラクタの描画
         self.draw('dino')
+        self.draw('enemys');
 
         //TODO あたり判定
 
@@ -67,15 +77,31 @@ class DinoGame {
             this.dino.moveY = -41
         }
     }
-    createEntity(name){
-        this[name] = {
-            x: this.img[name].width / 2,
-            y: this.canvas.height - this.img[name].height / 2,
+    createDino(){
+        this["dino"] = {
+            x: this.img["dino"].width / 2,
+            y: this.canvas.height - this.img["dino"].height / 2,
             moveY: 0,
+            width: this.img["dino"].width,
+            height: this.img["dino"].height,
+            image: this.img["dino"],
+            entityType: this.img["dino"].dataset.entity
+        }
+    }
+    createEnemys(name){
+        let enemyY = this.canvas.height - this.img[name].height / 2;
+        if(name === 'bird')enemyY = this.r(300 - this.img.bird.height) + 150;
+        let enemyMoveX = 1;
+        if(name === 'bird')enemyMoveX = -15;
+        if(name === 'cactus')enemyMoveX = -10;
+        this.enemys.push({
+            x: this.canvas.width + this.img[name].width / 2,
+            y: enemyY,
+            moveX: enemyMoveX,
             width: this.img[name].width,
             height: this.img[name].height,
             image: this.img[name]
-        }
+        });
     }
     move(name){
         if(name === 'dino'){
@@ -87,11 +113,28 @@ class DinoGame {
                 this.dino.moveY += 3;
             }
         }
+        if(name === 'enemys'){
+            for(const enemy of this.enemys){
+                enemy.x += enemy.moveX;
+            }
+
+            this.enemys = game.enemys.filter(enemy => enemy.x > -enemy.width);
+        }
     }
     draw(name){
-        this.ctx.drawImage(this.img[name],
-            this[name].x - this[name].width / 2,
-            this[name].y - this[name].height/ 2);
+        if(name === 'dino'){
+            this.ctx.drawImage(this.img[name],
+                this[name].x - this[name].width / 2,
+                this[name].y - this[name].height/ 2);
+        }
+        if(name === 'enemys'){
+            for (const enemy of this.enemys){
+                this.ctx.drawImage(enemy.image,
+                    enemy.x -enemy.width / 2,
+                    enemy.y -enemy.height /2
+                )
+            }
+        }
     }
 }
 const game = new DinoGame(canvas, imgNames);
