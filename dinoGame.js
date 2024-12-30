009215f0a40c82d653dfbd75d5958daa43c00ab1
drawell <eima_24s1101197@nnn.ed.jp>
2024-12-30 17:59:20 +0900

    Gameクラスに画像ロード処理追加&initメソッド追加

--- dinoGame.js
+++ dinoGame.js
@@ -9,13 +9,42 @@ const ctx = canvas.getContext('2d');
 const imgNames = ['bird', 'cactus', 'dino'];
 
 class Game {
-    constructor(){
+    /**
+     * @param {HTMLCanvasElement} canvas
+     * @param {string[]} imageNames 
+     */
+    constructor(canvas, imageNames){
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
         this.counter = 0;
         this.enemys = [];
         this.img = {};
         this.isGameOver = true;
         this.score = 0;
         this.timer = null;
+        let imgLoadCounter = 0;
+        for (const imgName of imageNames){
+            const imagePath = `image/${imgName}.png`;
+            this.img[imgName] = new Image();
+            this.img[imgName].src = imagePath;
+            this.img[imgName].onload = () => {
+                imgLoadCounter[0]++;
+                if(imgLoadCounter === imageNames.length){
+                    console.log('コンプリート：画像のロード');
+                    this.init();
+                }
+            }
+
+        }
+    }
+    init(){
+        this.counter = 0;
+        this.enemys = [];
+        this.isGameOver = false;
+        this.score = 0;
+        this.ctx.drawImage(this.img.bird, 500, 320);
+        this.ctx.drawImage(this.img.cactus, 300, 320);
+        this.ctx.drawImage(this.img.dino, 100, 320);
     }
 }
 const game = new Game();
