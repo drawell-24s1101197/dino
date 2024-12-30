655d394165405b930014225c6f610092e0693539
drawell <eima_24s1101197@nnn.ed.jp>
2024-12-30 17:41:05 +0900

    恐竜を表示させるプログラムを追加

--- dinoGame.js
+++ dinoGame.js
@@ -4,3 +4,9 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+//恐竜を表示させるプログラム
+const dinoImg = new Image();
+dinoImg.src = 'image/dino.png';
+dinoImg.onload = () => {
+    ctx.drawImage(dinoImg, 0, 320)
+}
