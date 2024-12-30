c0a91b4593e985fa566a4dba87fff3051be78731
drawell <eima_24s1101197@nnn.ed.jp>
2024-12-30 17:26:08 +0900

    canvasの処理を追加

---
+++ dinoGame.js
@@ -0,0 +1,8 @@
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'red';
+ctx.fillRect(30, 30, 150, 100);
