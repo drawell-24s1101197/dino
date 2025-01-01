af297043a9f03868e1a41041b0dda89af3a1f1dd
drawell <eima_24s1101197@nnn.ed.jp>
2025-01-01 16:21:35 +0900

    背景作成機能追加&滞空機能消去&あたり判定抜け機能追加

--- dinoGame.js
+++ dinoGame.js
@@ -18,11 +18,13 @@ class DinoGame {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
         this.counter = 0;
+        this.backGrounds = [];
         this.enemys = [];
         this.img = {};
         this.isGameOver = true;
         this.score = 0;
         this.timer = null;
+        this.canGameOver = true;
         let imgLoadCounter = 0;
         for (const imgDat of imageNames){
             const [imgName, imgAttr] = imgDat.split(':');
@@ -44,6 +46,8 @@ class DinoGame {
     init(){
         this.counter = 0;
         this.enemys = [];
+        this.backGrounds = [];
+        this.canGameOver = true;
         this.isGameOver = false;
         this.score = 0;
         this.createDino('dino');
@@ -56,19 +60,28 @@ class DinoGame {
     ticker(self){
         self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
 
+        //敵キャラ作成
         if(self.r(100 - self.score % 100) === 0)
             if(self.r(5) === 0)self.createEnemys('cactus');
         else if(self.r(100) === 0)self.createEnemys('bird');
+        //背景作成
+        if (self.counter % 10 === 0){
+            self.createBackGround();
+        }
 
         //キャラクタの移動
+        self.move('bg')
         self.move('dino');
         self.move('enemys');
 
         //キャラクタの描画
+        self.draw('bg')
         self.draw('dino')
         self.draw('enemys');
         self.draw('score');
 
+
+        //あたり判定
         self.hitCheck();
 
         //カウンタの更新
@@ -87,7 +100,7 @@ class DinoGame {
             })(0.1);
             this.init();
         };
-        if(e.code === 'KeyX')this.dino.moveY = -41;
+        if(e.code === 'KeyX')this.canGameOver = !this.canGameOver;
     }
     createDino(){
         this["dino"] = {
@@ -132,6 +145,22 @@ class DinoGame {
 
             this.enemys = game.enemys.filter(enemy => enemy.x > -enemy.width);
         }
+        if(name === 'bg'){
+            for (const bg of this.backGrounds){
+                bg.x += bg.moveX;
+            }
+        }
+    }
+    createBackGround(){
+        this.backGrounds = [];
+        for (let x = 0; x <= this.canvas.width; x+= 200){
+            this.backGrounds.push({
+                x: x,
+                y: canvas.height,
+                width: 200,
+                moveX: -20
+            });
+        }
     }
     draw(name){
         if(name === 'dino'){
@@ -148,11 +177,21 @@ class DinoGame {
             }
         }
         if(name === 'score'){
+            this.ctx.fillStyle = 'black';
             this.ctx.font = '24px serif';
             this.ctx.fillText(`score:${this.score}`, 0, 30);
         }
+        if(name === 'bg'){
+            this.ctx.fillStyle = 'sienna';
+            for (const bg of this.backGrounds){
+                this.ctx.fillRect(bg.x, bg.y - 5, bg.width, 5);
+                this.ctx.fillRect(bg.x + 20, bg.y - 10, bg.width - 40, 5);
+                this.ctx.fillRect(bg.x + 50, bg.y - 15, bg.width - 100, 5);
+            }
+        }
     }
     hitCheck(){
+        if(!this.canGameOver)return;
         const {x, y, width, height} = this.dino;
         for (const enemy of this.enemys){
             if(
@@ -160,6 +199,7 @@ class DinoGame {
                 Math.abs(y - enemy.y) < height * 0.5 / 2 + enemy.height * 0.9
             ){
                 this.isGameOver = true;
+                this.ctx.fillStyle = 'black';
                 this.ctx.font = 'bold 100px serif';
                 this.ctx.fillText('Game Over!', 150, 200);
                 clearInterval(this.timer);
